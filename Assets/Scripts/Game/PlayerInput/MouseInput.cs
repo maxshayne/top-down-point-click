90d49a6f91c523bef7b8221f0d5d03070f812a6d
ms <>
2024-05-18 02:18:09 +0700

    first folders structure, test click input with new input system

---
+++ Assets/Scripts/Game/PlayerInput/MouseInput.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Game.PlayerInput
+{
+    public class MouseInput : MonoBehaviour
+    {
+        [SerializeField] private Camera m_Camera;
+        [SerializeField] private Transform m_PlayerTransform;
+    }
+
+}
